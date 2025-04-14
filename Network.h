51479d0f805f4f0b780147866f3e9b2809a8ea29
wOwOv <regret6458@gmail.com>
2025-04-14 14:50:07 +0900

    Add files via upload

--- Network.h
+++ Network.h
@@ -1,21 +1,45 @@
 #ifndef __NETWORK__
 #define __NETWORK__
 
-#include "SerialBuffer.h"
+#include "RingBuffer.h"
+#include "winsock2.h"
 
-#define SERVERPORT 20000
+struct Player;
+struct SBuffer;
+struct Session;
+struct SectorAround;
+
+
+//player에게만 sendQ에 Enqueue
+//void SendUnicast(Player* player, SBuffer* buf);
+
+//player제외하고 전체sendQ에 Enqueue
+//void SendBroadcast(Player* player, SBuffer* buf);
+
+
+
+void networkStartUp();
+void networkCleanUp();
+
+void NetworkSelect();
+
+void AcceptProc();
+void RecvProc(Player* player);
+void SendProc(Player* player);
+
+void SendUnicast(Session* session, SBuffer* buf);
+void SendAround(Session* session, SBuffer* buf, bool me = false);
+void SendSectorList(Session* session, SectorAround* sectoraround, SBuffer* buf, bool me);
+void SendBroadcast(Session* session, SBuffer* buf);
 
 struct Session
 {
 	SOCKET sock;
-	SBuffer SendQ;
-	SBuffer RecvQ;
+	unsigned int Sessionid;
+	RingBuffer SendQ;
+	RingBuffer RecvQ;
 	unsigned int LastRecvTime;
-
-	unsigned int setnum;
 };
-
-
 
 
 #endif
